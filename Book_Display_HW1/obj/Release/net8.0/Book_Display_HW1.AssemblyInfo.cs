6b2963e6dadc8e66f7a8abf1ad62c222d88be84b
enessucu <enesimo41@gmail.com>
2024-03-06 00:19:40 +0300

    homework2 enes sucu

--- Book_Display_HW1/obj/Release/net8.0/Book_Display_HW1.AssemblyInfo.cs
+++ Book_Display_HW1/obj/Release/net8.0/Book_Display_HW1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Book_Display_HW1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a68ec8aab451df81fa5c7112d0857ea6c8a4ee45")]
 [assembly: System.Reflection.AssemblyProductAttribute("Book_Display_HW1")]
